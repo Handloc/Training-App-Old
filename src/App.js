895e105264d03ac55cedff7c3252393d7b4785f0
Maciej Handke <maciejhandke11@gmail.com>
2023-04-27 21:54:18 +0200

    Create basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <LoginPage /> },
+  { path: "/home", element: <HomePage /> },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
