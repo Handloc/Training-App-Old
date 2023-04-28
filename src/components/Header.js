7de2d83ae63f6a32d3c60230950ec7993ad97cf7
Maciej Handke <maciejhandke11@gmail.com>
2023-04-28 23:14:37 +0200

    Create simple header for Home Page

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="h-12 w-full fixed flex items-center justify-center border-b-2 border-slate-800">
+      <p className="text-3xl font-bold text-amber-400 font-marker">
+        Training App
+      </p>
+    </div>
+  );
+}
+
+export default Header;
