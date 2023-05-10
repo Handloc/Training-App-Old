abd039c8a593dd766f89db336b64b230d636302f
Maciej Handke <maciejhandke11@gmail.com>
2023-05-10 23:17:33 +0200

    Show header and hide form after successful user's sign in

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,16 @@
 import LoginForm from "../components/LoginForm/LoginForm";
+import { useSelector } from "react-redux";
+import Header from "../components/Header";
 
 function LoginPage() {
-  return <LoginForm />;
+  const authState = useSelector((state) => state.isAuthenticated);
+
+  return (
+    <>
+      {authState && <Header />}
+      {!authState && <LoginForm />}
+    </>
+  );
 }
 
 export default LoginPage;
